b8593cedc57cbb873012dba709a0285cf389c04f
Gustavo Prieto <vgdvx17@gmail.com>
2021-04-20 15:58:43 -0400

    update

--- lua/doom-barbar/init.lua
+++ lua/doom-barbar/init.lua
@@ -2,3 +2,4 @@
 vim.api.nvim_set_keymap('n', '<TAB>', ':BufferNext<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<S-TAB>', ':BufferPrevious<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>bd', ':BufferClose<CR>', { noremap = true, silent = true })
+
