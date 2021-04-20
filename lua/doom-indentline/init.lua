452779fd9b4f45dcfe2752f3fc1db6d2327ccdb8
Gustavo Prieto <vgdvx17@gmail.com>
2021-04-20 15:59:28 -0400

    indent

--- lua/doom-indentline/init.lua
+++ lua/doom-indentline/init.lua
@@ -9,3 +9,4 @@ vim.g.indent_blankline_context_patterns = {
     'arguments', 'if_statement', 'else_clause', 'jsx_element', 'jsx_self_closing_element', 'try_statement',
     'catch_clause', 'import_statement', 'operation_type'
 }
+
