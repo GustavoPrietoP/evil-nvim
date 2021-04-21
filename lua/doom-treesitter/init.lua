86abda0f5f5e594b6ebbb2e40c59fea91681db43
Gustavo Prieto <vgdvx17@gmail.com>
2021-04-21 12:12:00 -0400

    disabled tree-sitter indent

--- lua/doom-treesitter/init.lua
+++ lua/doom-treesitter/init.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
 	ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	highlight = {enable = true}, -- false will disable the whole extension
-	indent = { enable = true},
+	indent = { enable = false},
     autotag = {enable = false},
 	rainbow = {enable = true},
 }
