2c2a8fd69b3378af5ba81927dbd2270e66428f88
Gustavo Prieto <vgdvx17@gmail.com>
2021-04-22 19:34:36 -0400

    instructions on what each tree-sitter info does

--- lua/doom-treesitter/init.lua
+++ lua/doom-treesitter/init.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
 	ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	highlight = {enable = true}, -- false will disable the whole extension
-	indent = { enable = false},
-    autotag = {enable = false},
-	rainbow = {enable = true},
+	indent = { enable = false}, -- auto indent, no needs for this
+    autotag = {enable = false}, -- auto htlm tags
+	rainbow = {enable = true}, -- rainbow braces, false will disable it
 }
