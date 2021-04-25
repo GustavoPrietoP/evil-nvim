4cfd9e18a934ebe2d563ce7e3843a67fc5bcc656
Gustavo Prieto <vgdvx17@gmail.com>
2021-04-25 14:54:17 -0400

    test

--- lua/doom-plugins.lua
+++ lua/doom-plugins.lua
@@ -9,7 +9,28 @@ local install_path =  fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path) execute 'packadd packer.nvim' end
 
+--- Check if a file or directory exists in this path
+local function require_plugin(plugin)
+    local plugin_prefix = fn.stdpath("data") .. "/site/pack/packer/opt/"
+
+    local plugin_path = plugin_prefix .. plugin .. "/"
+    --	print('test '..plugin_path)
+    local ok, err, code = os.rename(plugin_path, plugin_path)
+    if not ok then
+        if code == 13 then
+            -- Permission denied, but it exists
+            return true
+        end
+    end
+    --	print(ok, err, code)
+    if ok then
+        vim.cmd("packadd " .. plugin)
+    end
+    return ok, err, code
+end
+
 vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
+
 
 return require('packer').startup(function(use)
   -- Packer can manage itself as an ional plugin
@@ -42,11 +63,13 @@ return require('packer').startup(function(use)
     use {'sbdchd/neoformat'}
 
 	-- Colorscheme
-    --use 'christianchiarulli/nvcode-color-schemes.vim'
-    use {"npxbr/gruvbox.nvim", requires=  {"rktjmp/lush.nvim"}}
+
+    use {'christianchiarulli/nvcode-color-schemes.vim'}
 	use {'norcalli/nvim-colorizer.lua'}
 	use {'GustavoPrietoP/doom-one.vim'}
 	use {'bluz71/vim-nightfly-guicolors'}
+    use {'tanvirtin/monokai.nvim'}
+
     -- Telescope
     use {'nvim-lua/popup.nvim'}
     use {'nvim-lua/plenary.nvim'}
@@ -78,3 +101,4 @@ return require('packer').startup(function(use)
 	-- Everyone needs a terminal
 	use {'akinsho/nvim-toggleterm.lua'}
 end)
+
