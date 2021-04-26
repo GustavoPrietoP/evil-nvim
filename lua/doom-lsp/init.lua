ee44427c728210bd01fe3f865351e37f3fc7215a
Gustavo Prieto <vgdvx17@gmail.com>
2021-04-26 18:01:01 -0400

    fixes lua lsp diagnostics not being able to find global variabel vi

--- lua/doom-lsp/init.lua
+++ lua/doom-lsp/init.lua
@@ -113,3 +113,23 @@ require('lspinstall').post_install_hook = function ()
   setup_servers() -- reload installed servers
   vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
 end
+
+
+
+
+-- Lsp Server settings --
+nvim_lsp["lua"].setup {
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            diagnostics = {globals = {"vim", "use"}},
+            workspace = {
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
+                },
+                maxPreload = 10000
+            }
+        }
+    }
+}
