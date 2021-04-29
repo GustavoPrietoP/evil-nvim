339ab93def37204e880a325346e6e30d77b3abf0
Gustavo Prieto <vgdvx17@gmail.com>
2021-04-29 17:30:27 -0400

    tree-sitter update

--- lua/doom-treesitter/init.lua
+++ lua/doom-treesitter/init.lua
@@ -1,7 +1,11 @@
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-	highlight = {enable = true}, -- false will disable the whole extension
-	indent = { enable = false}, -- auto indent, no needs for this
-    autotag = {enable = false}, -- auto htlm tags
-	rainbow = {enable = true}, -- rainbow braces, false will disable it
+    ensure_installed = evil.treesitter.ensure_installed, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ignore_install = evil.treesitter.ignore_install,
+    highlight = {
+        enable = evil.treesitter.highlight.enabled -- false will disable the whole extension
+    },
+    rainbow = {
+        enable = evil.treesitter.rainbow.enabled,
+        extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+    }
 }
