0bfcb155e5688a5cfc73ce9e56a7640777069cd0
Gustavo Prieto <vgdvx17@gmail.com>
2021-04-29 17:28:05 -0400

    added way more lua functions

--- lua/doom-settings.lua
+++ lua/doom-settings.lua
@@ -1,41 +1,53 @@
-vim.cmd('set colorcolumn=99999') -- fix indentline for now
 vim.cmd('set iskeyword+=-') -- treat dash separated words as a word text object"
 vim.cmd('set shortmess+=c') -- Don't pass messages to |ins-completion-menu|.
-vim.cmd('set noswapfile')
-vim.cmd('syntax on')
-vim.cmd('set nu')
 vim.cmd('set list listchars=tab:»·,trail:·')
 vim.cmd('inoremap {} {}<ESC>i')
 vim.cmd('inoremap {<RETURN> {}<ESC>i<CR><ESC>O') -- auto indent when for(){<Enter>
-vim.cmd [[ autocmd FileType * :norm '" ]] -- remove this if you dont want vim to preserve the last cursor editing position
-vim.o.hidden = true -- Required to keep multiple buffers open multiple buffers
-vim.o.expandtab = true
-vim.o.tabstop = 2
-vim.o.nu = true
-vim.o.relativenumber = true
-vim.o.scrolloff = 4
-vim.o.shiftwidth = 4
-vim.o.smartindent = true
-vim.o.autoindent = true
-vim.wo.wrap = false -- Display long lines as just one line
-vim.o.number = true
-vim.o.pumheight = 10 -- Makes popup menu smaller
-vim.o.fileencoding = "utf-8" -- The encoding written to file
-vim.o.mouse = "a" -- Enable your mouse
-vim.o.splitbelow = true -- Horizontal splits will automatically be below
-vim.o.autoread = true
-vim.o.termguicolors = true -- set term giu colors most terminals support this
-vim.o.splitright = true -- Vertical splits will automatically be to the right
-vim.o.t_Co = "256" -- Support 256 colors
-vim.o.conceallevel = 0 -- So that I can see `` in markdown files
-vim.wo.cursorline = true -- Enable highlighting of the current line
-vim.o.showtabline = 2 -- Always show tabs
-vim.o.showmode = false -- We don't need to see things like -- INSERT -- anymore
-vim.o.backup = false -- This is recommended by coc
-vim.o.writebackup = false -- This is recommended by coc
-vim.wo.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
-vim.o.updatetime = 100 -- Faster completion
-vim.o.timeoutlen = 200 -- By default timeoutlen is 1000 ms
-vim.o.clipboard = "unnamedplus" -- Copy paste between vim and everything else
-vim.o.cmdheight=1 -- increase this to make galaxyline move up
-vim.o.guifont = "SauceCodePro Nerd Font:h17"
+vim.cmd('syntax '..evil.syntax)
+vim.cmd('colorscheme '..evil.scheme)
+vim.o.background = evil.scheme_bg
+vim.o.termguicolors = evil.termcolors
+vim.o.cmdheight = evil.cmdheight
+vim.o.showmode = evil.showmode
+vim.o.hidden = evil.hidden
+vim.o.title = evil.title
+vim.o.wrap = evil.wrap
+vim.o.relativenumber = evil.relative
+vim.o.number = evil.number
+vim.o.splitbelow = evil.split_below
+vim.o.splitright = evil.split_right
+vim.o.pumheight = evil.pum_height
+vim.o.t_Co = evil.color_support
+vim.o.conceallevel = evil.conceallevel
+vim.o.cursorline = evil.line_highlight
+vim.wo.signcolumn = evil.sign_column
+vim.o.updatetime = evil.update_time
+vim.o.timeoutlen = evil.timeout_len
+vim.o.guifont = evil.guifont
+vim.bo.smartindent = evil.smart_indent
+vim.o.sw = evil.indent_spaces
+vim.o.tabstop = evil.soft_tabstop
+vim.o.softtabstop = evil.tabstop
+vim.o.autoindent = evil.autoindent
+vim.o.expandtab = evil.expand_tabs
+vim.g.colors_name = evil.scheme
+
+if evil.color_column == true then
+    vim.o.colorcolumn = evil.column_value
+end
+
+if evil.scrolloff == true then
+	vim.o.scrolloff = evil.scrolloff_count
+end
+
+if evil.clipboard == true then
+    vim.o.clipboard = "unnamedplus"
+end
+
+if evil.title == true then
+    vim.o.titlestring = evil.titlestring
+end
+
+if evil.mouse == true then
+    vim.o.mouse = "a"
+end
